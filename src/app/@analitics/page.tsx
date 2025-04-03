47716e25dc9b450b48cf30481eacfca693966c15
neominal.solutions <neominal.solutions@gmail.com>
2025-04-03 16:50:42 +0300

    Slots- Paralel Routes

---
+++ src/app/@analitics/page.tsx
@@ -0,0 +1,9 @@
+function Analitics() {
+	return (
+		<div style={{ padding: 10 }}>
+			<h1>Analitics Page</h1>
+		</div>
+	);
+}
+
+export default Analitics;
---
+++ src/app/@teams/page.tsx
@@ -0,0 +1,9 @@
+function Teams() {
+	return (
+		<div style={{ padding: 10 }}>
+			<h1>Team Page</h1>
+		</div>
+	);
+}
+
+export default Teams;
